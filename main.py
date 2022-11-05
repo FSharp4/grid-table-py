6cfb2b6a03a313c7d370f96dd9aec3fe70424aa9
FSharp4 <33583307+FSharp4@users.noreply.github.com>
2022-11-05 00:04:27 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# Press the green button in the gutter to run the script.
+import sys
+
+if __name__ == '__main__':
+    path = sys.argv[1]
+    outpath = sys.argv[2]
+    print(sys.argv.__len__())
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
